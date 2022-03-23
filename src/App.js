daa0921821acda257bd135870ba6c3ff37589d9e
Matthew Martin <matthew-martin@programmer.net>
2022-03-23 11:30:07 -0400

    Configured the route to the Home View Page.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,33 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { Container } from "reactstrap";
+import { Container,
+         Nav,
+         NavItem,
+         NavLink,
+         Row,
+         Col
+} from "reactstrap";
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Container>
-
+        <Nav>
+          <NavItem>
+            <NavLink href="/">
+              Home
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <Row>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Row>
       </Container>
     </Router>
   );
